685df6f6f21e5d25f041c792f06bd8bb6a1bb25d
samiiEng <mohammad.hossein.samii.iteng@gmail.com>
2022-07-18 22:51:28 +0430

    edit digikala.sql

--- digikala.sql
+++ digikala.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 18, 2022 at 06:45 PM
+-- Generation Time: Jul 18, 2022 at 08:20 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
